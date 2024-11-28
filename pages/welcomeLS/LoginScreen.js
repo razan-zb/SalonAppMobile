df776ffe9d24fd628a83fda2a5012e2bc10c6eb7
razan <razan@jasers-mbp.home>
2024-11-28 14:44:16 +0200

    Adding the LogIn logic and start styling

--- pages/welcomeLS/LoginScreen.js
+++ pages/welcomeLS/LoginScreen.js
@@ -1,24 +1,68 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
+import React, { useState } from 'react';
+import { Text, TextInput, TouchableOpacity, View, Image } from 'react-native';
+import * as SC from './mainPageStyling';
 
 const LoginScreen = () => {
+  // State to manage password visibility and checkbox state
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isChecked, setIsChecked] = useState(false); // For checkbox
+
+  // Toggle password visibility
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
+  };
+
+  // Toggle checkbox state
+  const toggleCheckbox = () => {
+    setIsChecked(!isChecked);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Log In Screen</Text>
-    </View>
+    <SC.LoginContainer>
+      <SC.LogInTitle>Welcome Back</SC.LogInTitle>
+      <SC.InnerContainer>
+        <View style={{ marginBottom: 20 }}>
+          <SC.LogInLabels>User Name</SC.LogInLabels>
+          <TextInput
+            style={SC.inputBox}
+            placeholder="Enter your email"
+            value={email}
+            onChangeText={setEmail}
+            keyboardType="email-address"
+          />
+        </View>
+
+        <View style={{ marginBottom: 20 }}>
+          <SC.LogInLabels>Password</SC.LogInLabels>
+          <TextInput
+            style={SC.inputBox} // Assuming you have an inputBox style defined
+            placeholder="Enter your password"
+            value={password}
+            onChangeText={setPassword}
+            secureTextEntry={!passwordVisible}
+          />
+        </View>
+
+        {/* Show password toggle */}
+        <TouchableOpacity onPress={togglePasswordVisibility} style={{ flexDirection: 'row', alignItems: 'center' }}>
+          <View style={{
+            width: 20,
+            height: 20,
+            borderWidth: 1,
+            borderColor: '#6C400A',
+            backgroundColor: passwordVisible ? '#6C400A' : 'white', // Checked/unchecked background color
+            marginRight: 10,
+            borderRadius: 3,
+          }} />
+          <SC.ShownPassword>{passwordVisible ? 'Hide Password' : 'Show Password'}</SC.ShownPassword>
+        </TouchableOpacity>
+
+      </SC.InnerContainer>
+    </SC.LoginContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
-});
 
 export default LoginScreen;
--- pages/welcomeLS/mainPageStyling.js
+++ pages/welcomeLS/mainPageStyling.js
@@ -1,7 +1,8 @@
+/* eslint-disable eol-last */
 import styled from 'styled-components/native';
 import { ImageBackground } from 'react-native';
 import mobileFrame from '../../assest/images/WelcomeFrameMobile.png';
-
+import ss from '../../assest/fonts/Crisa.otf';
 export const SafeAreaViewS = styled.SafeAreaView`
   flex: 1;
 `;
@@ -39,3 +40,52 @@ export const SecondaryText = styled.Text`
     margin-top: 1px;
 
 `;
+
+//LogIn
+export const LoginContainer = styled.View`
+    flex:1;
+    justify-content: center; 
+    align-items: center; 
+    background-color:#E6A556;
+    width:100%;
+    higth:100%
+`;
+
+export const InnerContainer = styled.View`
+    background-color:#F6C992;
+    width:350px;
+    height:300px;
+    padding:10px;
+    margin-top:20px;
+    justify-content: center;
+
+
+
+`;
+
+export const LogInTitle = styled.Text`
+    font-size:30px;
+    font-weight: bold;
+    color:#6C400A;
+    font-family: cursive;
+
+
+`;
+
+export const LogInLabels = styled.Text`
+    font-size:20px;
+    font-weight: bold;
+    color:#6C400A;
+    font-family: cursive;
+
+`;
+
+export const ShownPassword = styled.Text`
+    font-size:16px;
+    font-weight: bold;
+    color:#6C400A;
+    font-family: cursive;
+
+`;
+
+
