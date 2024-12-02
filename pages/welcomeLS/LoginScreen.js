1ba098d8424a1da8023f013bdaf25a6d38891b4f
razan <razan@jasers-mbp.home>
2024-12-02 12:37:44 +0200

    LogIn

--- pages/welcomeLS/LoginScreen.js
+++ pages/welcomeLS/LoginScreen.js
@@ -1,33 +1,38 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, { useState } from 'react';
-import { Text, TextInput, TouchableOpacity, View, Image } from 'react-native';
+import { TouchableOpacity, View } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons'; 
 import * as SC from './mainPageStyling';
 
-const LoginScreen = () => {
-  // State to manage password visibility and checkbox state
+const LoginScreen = ({ navigation }) => {
   const [passwordVisible, setPasswordVisible] = useState(false);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [isChecked, setIsChecked] = useState(false); // For checkbox
 
-  // Toggle password visibility
   const togglePasswordVisibility = () => {
     setPasswordVisible(!passwordVisible);
   };
 
-  // Toggle checkbox state
-  const toggleCheckbox = () => {
-    setIsChecked(!isChecked);
+  const handleBackPress = () => {
+    navigation.goBack();
+  };
+
+  const handleLoginPress = () => {
+    alert('Login button pressed');
   };
 
   return (
     <SC.LoginContainer>
+      <SC.BackArrow
+        onPress={handleBackPress}>
+        <Icon name="arrow-back" size={30} color="#6C400A" />
+      </SC.BackArrow>
+
       <SC.LogInTitle>Welcome Back</SC.LogInTitle>
       <SC.InnerContainer>
         <View style={{ marginBottom: 20 }}>
           <SC.LogInLabels>User Name</SC.LogInLabels>
-          <TextInput
-            style={SC.inputBox}
+          <SC.InputBox
             placeholder="Enter your email"
             value={email}
             onChangeText={setEmail}
@@ -37,8 +42,7 @@ const LoginScreen = () => {
 
         <View style={{ marginBottom: 20 }}>
           <SC.LogInLabels>Password</SC.LogInLabels>
-          <TextInput
-            style={SC.inputBox} // Assuming you have an inputBox style defined
+          <SC.InputBox
             placeholder="Enter your password"
             value={password}
             onChangeText={setPassword}
@@ -46,21 +50,18 @@ const LoginScreen = () => {
           />
         </View>
 
-        {/* Show password toggle */}
         <TouchableOpacity onPress={togglePasswordVisibility} style={{ flexDirection: 'row', alignItems: 'center' }}>
-          <View style={{
-            width: 20,
-            height: 20,
-            borderWidth: 1,
-            borderColor: '#6C400A',
-            backgroundColor: passwordVisible ? '#6C400A' : 'white', // Checked/unchecked background color
-            marginRight: 10,
-            borderRadius: 3,
-          }} />
+          <SC.Checkbox checked={passwordVisible} />
           <SC.ShownPassword>{passwordVisible ? 'Hide Password' : 'Show Password'}</SC.ShownPassword>
         </TouchableOpacity>
-
       </SC.InnerContainer>
+
+      {/* Login Button */}
+      <TouchableOpacity onPress={handleLoginPress} style={{ marginTop: 20 }}>
+        <SC.LoginButton>
+          <SC.ButtonText>Login</SC.ButtonText>
+        </SC.LoginButton>
+      </TouchableOpacity>
     </SC.LoginContainer>
   );
 };
--- pages/welcomeLS/mainPageStyling.js
+++ pages/welcomeLS/mainPageStyling.js
@@ -2,7 +2,6 @@
 import styled from 'styled-components/native';
 import { ImageBackground } from 'react-native';
 import mobileFrame from '../../assest/images/WelcomeFrameMobile.png';
-import ss from '../../assest/fonts/Crisa.otf';
 export const SafeAreaViewS = styled.SafeAreaView`
   flex: 1;
 `;
@@ -64,10 +63,9 @@ export const InnerContainer = styled.View`
 `;
 
 export const LogInTitle = styled.Text`
-    font-size:30px;
+    font-size:24px;
     font-weight: bold;
     color:#6C400A;
-    font-family: cursive;
 
 
 `;
@@ -76,7 +74,6 @@ export const LogInLabels = styled.Text`
     font-size:20px;
     font-weight: bold;
     color:#6C400A;
-    font-family: cursive;
 
 `;
 
@@ -87,5 +84,45 @@ export const ShownPassword = styled.Text`
     font-family: cursive;
 
 `;
+
+
+export const InputBox = styled.TextInput`
+    font-size:16px;
+    color:#6C400A;
+    border: 0; 
+
+`;
+
+export const Checkbox = styled.View`
+  width: 20px;
+  height: 20px;
+  border-width: 1px;
+  border-color: #6c400a;
+  margin-right: 10px;
+  border-radius: 3px;
+  background-color: ${(props) => (props.checked ? '#6C400A' : 'white')};
+`;
+
+export const LoginButton = styled.TouchableOpacity`
+  background-color: #6C400A;
+  padding: 15px 20px;
+  border-radius: 5px;
+  align-items: center;
+  margin-top: 30px;
+`;
+
+export const LoginButtonText = styled.Text`
+  color: white;
+  font-size: 16px;
+  font-weight: bold;
+`;
+
+export const BackArrow = styled.TouchableOpacity`
+  position:absolute;
+  align-self:right;
+  top:20px;  
+`;
+
+
 
 
